84b3412b1196d433dddde864b6897fddc94e67da
Youneeesss <oumhamed.younes.mpsi@gmail.com>
2023-05-30 01:10:15 +0200

    Rouge eval

--- rouge_scores.py
+++ rouge_scores.py
@@ -7,20 +7,17 @@ def rouge_scores(filename):
 
     # Split the content into test samples
     samples = content.split("\n\n")
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+    scorer = rouge_scorer.RougeScorer(['rouge2', 'rougeL'], use_stemmer=True)
 
     for sample in samples:
         lines = sample.strip().split("\n")
-        generated_summary = lines[5]
-        original_summary = lines[3]
+        generated_summary = lines[5].replace('<pad>', '').replace('<sos>', '').replace('<eof>', '')
+        original_summary = lines[3].replace('<pad>', '').replace('<sos>', '').replace('<eof>', '')
 
         scores = scorer.score(generated_summary, original_summary)
         lines[1] = lines[1].replace("<eos>", "").replace("<pad>", "").replace("<sos>", "")
         with open(f'scores_{filename}', "a") as f:
-            f.write(f"Rouge Score for {lines[0]}:\n{lines[1]}\n")
-            f.write(f"Rouge-1: {scores['rouge1']} \n")
-            f.write(f"Rouge-L: {scores['rougeL']}")
+            f.write(f"Rouge-2: {scores['rouge2']} \n")
             f.write("\n\n")
 
-rouge_scores('results_topk.txt')
 rouge_scores('results_topp.txt')
